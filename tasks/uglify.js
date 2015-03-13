1f58e1f4cebe60900bba564d2f95a50b53d96c8d
Zatvor <a.zatvornitskiy@gmail.com>
2015-03-13 20:32:28 +0200

    This resolves #3. Add source map file

--- tasks/uglify.js
+++ tasks/uglify.js
@@ -1,14 +1,10 @@
 module.exports = {
   options: {
-    report: 'min',
-    wrap: true
+    report: 'min', wrap: true, sourceMap: true, mangle: false
   },
   build: {
     files: [
-      {
-        src: 'ember-insights.amd.js',
-        dest: 'ember-insights.amd.min.js',
-      },
+      { src: 'ember-insights.amd.js', dest: 'ember-insights.amd.min.js' },
     ],
   }
 };
